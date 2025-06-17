2b17fff1914626964205cc48414cda2f37758793
lasAKAKara <lebach.kara@gmail.com>
2025-06-17 15:21:35 +0700

    L10-done

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ const app = Vue.createApp({
             this.cart.push(id)
         },
         removeItem(id){
-            this.cart.pop(id)
+            const index = this.cart.indexOf(id)
+            if(index > -1){
+                this.cart.splice(index)
+            }
         }
     }
 })
